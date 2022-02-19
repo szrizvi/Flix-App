8306858855b94a553547ac66e5aa40ca4938de4c
szrizvi <77036574+szrizvi@users.noreply.github.com>
2022-02-19 11:34:12 -0800

    Initial Commit

---
+++ Flix Clone Assignment 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix Clone Assignment 1
+//
+//  Created by Zainab Rizvi on 19/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
